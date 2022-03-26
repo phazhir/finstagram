1bd39dea1ace6365755e9f2f87a7cf692a0fcb03
phazhir <p.hazhir@gmail.com>
2022-03-26 22:34:22 +0000

    W4D1

--- app/actions.rb
+++ app/actions.rb
@@ -1,49 +1,12 @@
-def humanized_time_ago(time_ago_in_minutes)
-    if time_ago_in_minutes >= 60
-        "#{time_ago_in_minutes / 60} hours ago"
+def humanized_time_ago(minute_num)
+    if minute_num >= 60
+        "#{minute_num / 60} hours ago"
     else
-        "#{time_ago_in_minutes} minutes ago"
+        "#{minute_num} minutes ago"
     end
 end
-
 get '/' do 
-    finstagram_post_shark = {
-        username: "sharky_j",
-        avatar_url: "http://naserca.com/images/sharky_j.jpg",
-        photo_url: "http://naserca.com/images/sharky_j.jpg",
-        humanized_time_ago: humanized_time_ago(15),
-        like_count: 0,
-        comment_count: 1,
-        comments: [{
-            username: "sharky_j",
-            text: "Out for the long weekend...excited to show y'all the beach bod"
-        }]
-    }
 
-    finstgram_post_whale = {
-        username: "kirk whalum",
-        avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
-        photo_url: "http://naserca.com/images/whale.jpg",
-        humanized_time_ago: humanized_time_ago(65),
-        like_count: 0,
-        comment_count: 1,
-        comments: [{
-            username: "kirk whalum",
-            text: "#weekendvibes"
-        }]
-    }
-
-    finstagram_post_marlin = {
-        username: "marlin peppa",
-        avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
-        photo_url: "http://naserca.com/images/marlin.jpg",
-        humanized_time_ago: humanized_time_ago(190),
-        like_count: 0,
-        comment_count: 1,
-        comments: [{
-            username: "marlin peppa",
-            text: "lunchtime! ;)"
-        }]
-    }
-    [finstagram_post_shark, finstgram_post_whale, finstagram_post_marlin].to_s
+    @finstagram_posts = FinstagramPost.order(created_at: :desc)
+    erb(:index)
 end
