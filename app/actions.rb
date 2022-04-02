c7cf334e03631ccbe21b2761c5b66407366c54ec
phazhir <p.hazhir@gmail.com>
2022-04-02 22:31:06 +0000

    W5D1

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,9 @@
+helpers do
+    def current_user 
+        User.find_by(id: session[:user_id])
+    end 
+end
+
 def humanized_time_ago(minute_num)
     if minute_num >= 60
         "#{minute_num / 60} hours ago"
@@ -9,4 +15,54 @@ get '/' do
 
     @finstagram_posts = FinstagramPost.order(created_at: :desc)
     erb(:index)
+end
+
+get '/signup' do
+    @user = User.new
+    erb(:signup)
+end
+
+post '/signup' do
+    email      = params[:email]
+    avatar_url = params[:avatar_url]
+    username   = params[:username]
+    password   = params[:password]
+
+    @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password})
+
+    if @user.save
+
+        redirect to('/login')
+    else
+        erb(:signup)
+    end
+
+end
+
+
+get '/login' do 
+    erb(:login)
+end
+
+post '/login' do
+    username = params[:username]
+    password = params[:password]
+
+    user = User.find_by(username: username)
+
+    if user && user.password == password 
+        session[:user_id] = user.id 
+        redirect to('/')
+
+    else
+       @error_message = "Login Failed"
+       erb(:login)
+
+    end
+    
+end 
+
+get '/logout' do 
+    session[:user_id] = nil 
+    redirect to('/')
 end
