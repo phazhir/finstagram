66cb7950773bf7a5bc4d5ae730dca4d4f045e970
phazhir <p.hazhir@gmail.com>
2022-04-10 20:35:13 +0000

    pre-deployment

--- app/actions.rb
+++ app/actions.rb
@@ -75,7 +75,7 @@ end
 post '/finstagram_posts' do
     photo_url = params[:photo_url]
     
-    @finstagram_post = FinstagramPost.new({ photo_url: photo_url, user_id: current_user.id})
+    @finstagram_post = FinstagramPost.new({ photo_url: photo_url, user_id: current_user.id })
     
     if @finstagram_post.save
         redirect(to('/')) 
@@ -88,4 +88,31 @@ get '/finstagram_posts/:id' do
     @finstagram_post = FinstagramPost.find(params[:id])
     erb(:"finstagram_posts/show")
     
+end 
+
+post '/comments' do 
+    text = params[:text] 
+    finstagram_post_id = params[:finstagram_post_id] 
+
+    comment = Comment.new({ text: text, finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+
+    comment.save 
+
+    redirect(back)
+
+end 
+
+post '/likes' do 
+    finstagram_post_id = params[:finstagram_post_id]
+
+    like = Like.new({ finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+    like.save
+
+    redirect(back) 
+end 
+
+delete '/likes/:id' do 
+    like = Like.find(params[:id]) 
+    like.destroy 
+    redirect(back) 
 end 
